c80909815a880fa306cc66ca5101dccfeee6415c
SardinasJL <sardinasjoseluis1@gmail.com>
2022-01-15 23:40:35 -0400

    formulario y algoritmos
    Se crearon los principales algoritmos y el formulario principal

---
+++ herramientas/Form1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtRegex = New System.Windows.Forms.TextBox()
+        Me.txtCadena = New System.Windows.Forms.TextBox()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(278, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Comprobar expresiones regulares"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(581, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "1. Introduzca una expresión regular, sin comillas, por ejemplo: [0-9]{1,3}"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 49)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(324, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "2. Introduzca una cadena, a comprobar"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(156, 301)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(154, 20)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Expresión regular:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(156, 347)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(76, 20)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Cadena:"
+        '
+        'txtRegex
+        '
+        Me.txtRegex.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRegex.Location = New System.Drawing.Point(316, 298)
+        Me.txtRegex.Name = "txtRegex"
+        Me.txtRegex.Size = New System.Drawing.Size(365, 26)
+        Me.txtRegex.TabIndex = 0
+        '
+        'txtCadena
+        '
+        Me.txtCadena.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCadena.Location = New System.Drawing.Point(316, 344)
+        Me.txtCadena.Name = "txtCadena"
+        Me.txtCadena.Size = New System.Drawing.Size(365, 26)
+        Me.txtCadena.TabIndex = 1
+        '
+        'lblResultado
+        '
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResultado.ForeColor = System.Drawing.Color.Red
+        Me.lblResultado.Location = New System.Drawing.Point(156, 387)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(96, 20)
+        Me.lblResultado.TabIndex = 7
+        Me.lblResultado.Text = "Resultado:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(12, 81)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 20)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "OJO:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(12, 104)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(707, 20)
+        Me.Label8.TabIndex = 10
+        Me.Label8.Text = "- En Visual Basic la regex debe empezar con ^ y terminar en $, por ejemplo: ^[0-9" & _
+    "]{1,3}$"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(12, 129)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(692, 20)
+        Me.Label9.TabIndex = 11
+        Me.Label9.Text = "- En Larevel, la regex debe empezar con /^ y terminar en $/, por ejemplo: /^[0-9]" & _
+    "{1,3}$/"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(12, 197)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(682, 20)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "- En JavaScript, la regex debe empezar con / y terminar en /, por ejemplo: /[0-9]" & _
+    "{1,3}/"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(12, 225)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(601, 20)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "- En HTML5, no tiene un caracter de inicio ni de fin, por ejemplo: [0-9]{1,3}"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(80, 149)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(639, 20)
+        Me.Label11.TabIndex = 14
+        Me.Label11.Text = "- En Larevel, se recomienda usar un array cuando se utiliza regex, por ejemplo:"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(141, 169)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(486, 20)
+        Me.Label12.TabIndex = 15
+        Me.Label12.Text = """precio"" => [""required"", ""regex:/^[0-9]{1,4}\.{0,1}[0-9]{0,2}$/""]"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(904, 446)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblResultado)
+        Me.Controls.Add(Me.txtCadena)
+        Me.Controls.Add(Me.txtRegex)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtRegex As System.Windows.Forms.TextBox
+    Friend WithEvents txtCadena As System.Windows.Forms.TextBox
+    Friend WithEvents lblResultado As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+
+End Class
---
+++ herramientas/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Text.RegularExpressions
+Public Class Form1
+    Public Sub verificar()
+        Try
+            If Regex.IsMatch(txtCadena.Text, txtRegex.Text) = True Then
+                lblResultado.Text = "Resultado: La cadena coincide con la expresión regular √"
+                lblResultado.ForeColor = Color.ForestGreen
+            Else
+                lblResultado.Text = "Resultado: La cadena NO coincide con la expresión regular X"
+                lblResultado.ForeColor = Color.Red
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub txtRegex_KeyUp(sender As Object, e As KeyEventArgs) Handles txtRegex.KeyUp
+        verificar()
+    End Sub
+
+    Private Sub txtCadena_KeyUp(sender As Object, e As KeyEventArgs) Handles txtCadena.KeyUp
+        verificar()
+    End Sub
+
+    
+   
+End Class
